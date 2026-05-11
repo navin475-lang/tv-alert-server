c95757aaef9290be4f5418be011dbf72b22d63a1
navin475-lang <navin475@gmail.com>
2026-05-11 13:19:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-```python
 from flask import Flask, request
 import smtplib
 import requests
@@ -109,4 +108,3 @@ Time   : {time_now}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-```
